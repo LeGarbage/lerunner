3324fdf15cbb2a84adc3a2613daa88e96ece25bc
Logan Lessen <loganlessen@gmail.com>
2026-07-07 19:19:02 -0600

    searching works now??

--- src/window.cpp
+++ src/window.cpp
@@ -5,6 +5,7 @@
 #include <gtkmm/eventcontrollerfocus.h>
 #include <gtkmm/eventcontrollerkey.h>
 #include <iostream>
+#include <print>
 
 MainWindow::MainWindow() {
     set_default_size(640, 0);
@@ -16,19 +17,8 @@ MainWindow::MainWindow() {
     box.append(entry);
     entry.set_placeholder_text("Search");
 
+    // TODO: Add automatic plugin loading
     plugins.push_back(std::make_unique<DesktopEntries>());
-
-    for (const auto& entry : plugins[0]->get_entries("S")) {
-        auto &button = desktop_app_buttons.emplace_back(entry->display());
-
-        button.signal_clicked().connect([this, entry] {
-            entry->selected();
-            unset_focus();
-        });
-        box.append(button);
-        // button.set_visible(false);
-        button.set_expand();
-    }
 
     auto key_controller = Gtk::EventControllerKey::create();
     key_controller->signal_key_pressed().connect(sigc::mem_fun(*this, &MainWindow::on_key_pressed),
@@ -46,31 +36,29 @@ void MainWindow::on_button_clicked(const std::string &data) {
 }
 
 void MainWindow::on_search_changed() {
-    // FIX: WTF??
-    //
-    // auto text = entry.get_text().lowercase();
-    // for (auto &button : desktop_app_buttons) {
-    //     box.remove(button);
-    // }
-    //
-    // desktop_app_buttons.clear();
-    //
-    // for (auto &button : desktop_app_buttons) {
-    //     if (text.length() != 0 && button.get_label().lowercase().rfind(text, 0) == 0) {
-    //         button.set_visible();
-    //     } else {
-    //         button.set_visible(false);
-    //     }
-    // }
-    // for (auto *const entry : plugins[0]->get_entries(text)) {
-    //     // if (entry.get("Desktop Entry/NoDisplay") == "true") { continue; }
-    //
-    //     auto &button = desktop_app_buttons.emplace_back(entry->display());
-    //
-    //     button.signal_clicked().connect([&entry]() { entry->selected(); });
-    //     box.append(button);
-    //     button.set_expand();
-    // }
+    auto text = entry.get_text();
+
+    for (auto &button : desktop_app_buttons) {
+        box.remove(button);
+    }
+
+    desktop_app_buttons.clear();
+
+    for (auto *const entry : plugins[0]->get_entries(text)) {
+        // if (entry.get("Desktop Entry/NoDisplay") == "true") { continue; }
+
+        auto &button = desktop_app_buttons.emplace_back(entry->display());
+
+        button.signal_clicked().connect([this, entry] {
+            entry->selected();
+            unset_focus();
+        });
+
+        box.append(button);
+        button.set_expand();
+    }
+
+    std::println("{}", box.get_children().size());
 }
 
 bool MainWindow::on_key_pressed(guint keyval, guint keycode, Gdk::ModifierType state) {
